0d5175b5cff6abc87f05a484d16279265b67b274
Robson Bettini <robson.bettini@hotmail.com>
2022-07-07 15:52:36 -0300

    Finished challenge bds11

--- frontweb/src/pages/Admin/Employees/Form/index.tsx
+++ frontweb/src/pages/Admin/Employees/Form/index.tsx
@@ -1,9 +1,58 @@
+import { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import Select from 'react-select';
+import { Department } from 'types/department';
+import { Employee } from 'types/employee';
+import { requestBackend } from 'util/requests';
+import { useHistory } from 'react-router-dom';
+
+import { toast } from 'react-toastify';
+
 import './styles.css';
 
 const Form = () => {
+  const history = useHistory();
+
+  const [selectDepartments, setSelectDepartments] = useState<Department[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<Employee>();
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/departments',
+      withCredentials: true,
+    };
+    requestBackend(config).then((response) =>
+      setSelectDepartments(response.data)
+    );
+  }, []);
+
+  const onSubmit = (formData: Employee) => {
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/employees',
+      data: formData,
+      withCredentials: true,
+    };
+    requestBackend(config)
+      .then(() => {
+        toast.info('Cadastrado com sucesso');
+        history.push('/admin/employees');
+      })
+      .catch((response) => {
+        toast.error('Erro ao cadastrar');
+      });
+  };
 
   const handleCancel = () => {
-    // to do
+    history.push('/admin/employees');
   };
 
   return (
@@ -11,28 +60,77 @@ const Form = () => {
       <div className="base-card employee-crud-form-card">
         <h1 className="employee-crud-form-title">INFORME OS DADOS</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)} data-testid="form">
           <div className="row employee-crud-inputs-container">
             <div className="col employee-crud-inputs-left-container">
-
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input is-invalid"
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text" className={`form-control base-input ${
+                    errors.name ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome do funcionário"
+                  name="name"
+                  data-testid="name"
                 />
+
                 <div className="invalid-feedback d-block">
-                  Mensagem de erro
+                  {errors.name?.message}
                 </div>
               </div>
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input"
+                <input
+                  {...register('email', {
+                    required: 'Campo obrigatório',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Email inválido',
+                    },
+                  })}
+                  type="text" className={`form-control base-input ${
+                    errors.email ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Email do funcionário"
+                  name="email"
+                  data-testid="email"
                 />
                 <div className="invalid-feedback d-block">
-                  
+                  {errors.email?.message}
                 </div>
               </div>
 
+              <div className="margin-botttom-30">
+                <label htmlFor="department" className="d-none">
+                  Departamento
+                </label>
+                <Controller
+                  name="department"
+                  rules={{ required: true }}
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      options={selectDepartments}
+                      classNamePrefix="product-crud-select"
+                      getOptionLabel={(department: Department) =>
+                        department.name
+                      }
+                      getOptionValue={(department: Department) =>
+                        String(department.id)
+                      }
+                      inputId="department"
+                    />
+                  )}
+                />
+                {errors.department && (
+                  <div className="invalid-feedback d-block">
+                    Campo obrigatório
+                  </div>
+                )}
+              </div>
             </div>
           </div>
           <div className="employee-crud-buttons-container">
--- frontweb/src/pages/Admin/Employees/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Employees/__tests__/Form.spec.tsx
@@ -1,10 +1,10 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import Form from "../Form";
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Form from '../Form';
 import history from 'util/history';
 import { Router } from 'react-router-dom';
-import { server } from "./fixtures";
-import selectEvent from "react-select-event";
+import { server } from './fixtures';
+import selectEvent from 'react-select-event';
 import { ToastContainer } from 'react-toastify';
 
 beforeAll(() => server.listen());
@@ -12,115 +12,109 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe('Employee form create tests', () => {
+  test('should show toast and redirect when submit form correctly', async () => {
+    render(
+      <Router history={history}>
+        <ToastContainer />
+        <Form />
+      </Router>
+    );
 
-    test('should show toast and redirect when submit form correctly', async () => {
+    const nameInput = screen.getByTestId('name');
+    const emailInput = screen.getByTestId('email');
+    const categoriesInput = screen.getByLabelText('Departamento');
 
-        render(
-            <Router history={history}>
-                <ToastContainer />
-                <Form />
-            </Router>
-        );
-    
-        const nameInput = screen.getByTestId("name");
-        const emailInput = screen.getByTestId("email");
-        const categoriesInput = screen.getByLabelText("Departamento");
+    await selectEvent.select(categoriesInput, 'Sales');
+    userEvent.type(nameInput, 'Abel');
+    userEvent.type(emailInput, 'abel@gmail.com');
 
-        await selectEvent.select(categoriesInput, 'Sales');
-        userEvent.type(nameInput, 'Abel');
-        userEvent.type(emailInput, 'abel@gmail.com');
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+    userEvent.click(submitButton);
 
-        const submitButton = screen.getByRole('button', { name: /salvar/i})
-        userEvent.click(submitButton);
-
-        await waitFor(() => {
-            const toastElement = screen.getByText('Cadastrado com sucesso');
-            expect(toastElement).toBeInTheDocument();
-        });
-
-        expect(history.location.pathname).toEqual('/admin/employees');
+    await waitFor(() => {
+      const toastElement = screen.getByText('Cadastrado com sucesso');
+      expect(toastElement).toBeInTheDocument();
     });
 
-    test('should show 3 error messages when just clicking submit', async () => {
+    expect(history.location.pathname).toEqual('/admin/employees');
+  });
 
-        render(
-            <Router history={history}>
-                <Form />
-            </Router>
-        );
-    
-        const submitButton = screen.getByRole('button', { name: /salvar/i})
-        userEvent.click(submitButton);
+  test('should show 3 error messages when just clicking submit', async () => {
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
 
-        await waitFor(() => {
-            const messages = screen.getAllByText('Campo obrigatório');
-            expect(messages).toHaveLength(3);
-        });
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const messages = screen.getAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(3);
+    });
+  });
+
+  test('should validate email syntax', async () => {
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
+
+    const emailInput = screen.getByTestId('email');
+    userEvent.type(emailInput, 'abel@gmail.');
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const message = screen.getByText('Email inválido');
+      expect(message).toBeInTheDocument();
+    });
+  });
+
+  test('should clear error messages when filling out the form', async () => {
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
+
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const messages = screen.getAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(3);
     });
 
-    test('should validate email syntax', async () => {
+    const nameInput = screen.getByTestId('name');
+    const emailInput = screen.getByTestId('email');
+    const categoriesInput = screen.getByLabelText('Departamento');
 
-        render(
-            <Router history={history}>
-                <Form />
-            </Router>
-        );
-    
-        const emailInput = screen.getByTestId("email");
-        userEvent.type(emailInput, 'abel@gmail.');
+    await selectEvent.select(categoriesInput, 'Sales');
+    userEvent.type(nameInput, 'Abel');
+    userEvent.type(emailInput, 'abel@gmail.com');
 
-        const submitButton = screen.getByRole('button', { name: /salvar/i})
-        userEvent.click(submitButton);
-
-        await waitFor(() => {
-            const message = screen.getByText('Email inválido');
-            expect(message).toBeInTheDocument();
-        });
+    await waitFor(() => {
+      const messages = screen.queryAllByText('Campo obrigatório');
+      expect(messages).toHaveLength(0);
     });
+  });
 
-    test('should clear error messages when filling out the form', async () => {
+  test('should redirect when canceling', async () => {
+    render(
+      <Router history={history}>
+        <Form />
+      </Router>
+    );
 
-        render(
-            <Router history={history}>
-                <Form />
-            </Router>
-        );
-    
-        const submitButton = screen.getByRole('button', { name: /salvar/i})
-        userEvent.click(submitButton);
+    const cancelButton = screen.getByRole('button', { name: /cancelar/i });
+    userEvent.click(cancelButton);
 
-        await waitFor(() => {
-            const messages = screen.getAllByText('Campo obrigatório');
-            expect(messages).toHaveLength(3);
-        });
-
-        const nameInput = screen.getByTestId("name");
-        const emailInput = screen.getByTestId("email");
-        const categoriesInput = screen.getByLabelText("Departamento");
-
-        await selectEvent.select(categoriesInput, 'Sales');
-        userEvent.type(nameInput, 'Abel');
-        userEvent.type(emailInput, 'abel@gmail.com');
-
-        await waitFor(() => {
-            const messages = screen.queryAllByText('Campo obrigatório');
-            expect(messages).toHaveLength(0);
-        });
+    await waitFor(() => {
+      expect(history.location.pathname).toEqual('/admin/employees');
     });
-
-    test('should redirect when canceling', async () => {
-
-        render(
-            <Router history={history}>
-                <Form />
-            </Router>
-        );
-    
-        const cancelButton = screen.getByRole('button', { name: /cancelar/i})
-        userEvent.click(cancelButton);
-
-        await waitFor(() => {
-            expect(history.location.pathname).toEqual('/admin/employees');
-        });
-    });
+  });
 });
